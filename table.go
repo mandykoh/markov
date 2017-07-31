66649103a5aaffe29479cd8aba36c9eeabf985ad
Amanda Koh <amanda@naucera.net>
2017-08-01 08:17:42 +1000

    Keep entries sorted.

--- table.go
+++ table.go
@@ -23,6 +23,23 @@ func (t *Table) Add(s Symbol) {
 
 	t.Entries[index].Frequency++
 	t.TotalFrequency++
+
+	t.sortEntry(index)
+}
+
+func (t *Table) sortEntry(index int) {
+	j := index
+
+	for i := index - 1; i >= 0 && t.Entries[j].Frequency > t.Entries[i].Frequency; i-- {
+		t.EntryIndices[t.Entries[i].Symbol.Key()] = j
+		t.EntryIndices[t.Entries[j].Symbol.Key()] = i
+
+		tmp := t.Entries[i]
+		t.Entries[i] = t.Entries[j]
+		t.Entries[j] = tmp
+
+		j = i
+	}
 }
 
 func EmptyTable() Table {
--- table_test.go
+++ table_test.go
@@ -1,6 +1,8 @@
 package markov
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestTable(t *testing.T) {
 
@@ -10,10 +12,12 @@ func TestTable(t *testing.T) {
 			table := EmptyTable()
 			table.Add(StringSymbol("a"))
 
+			indexA := table.EntryIndices[SymbolKey("a")]
+
 			if actual := len(table.Entries); actual != 1 {
 				t.Fatalf("Expected 1 entry but got %d", actual)
 			}
-			if expected, actual := uint64(1), table.Entries[0].Frequency; expected != actual {
+			if expected, actual := uint64(1), table.Entries[indexA].Frequency; expected != actual {
 				t.Fatalf("Expected frequency of %d but got %d", expected, actual)
 			}
 			if expected, actual := uint64(1), table.TotalFrequency; expected != actual {
@@ -21,11 +25,12 @@ func TestTable(t *testing.T) {
 			}
 
 			table.Add(StringSymbol("b"))
+			indexB := table.EntryIndices[SymbolKey("b")]
 
 			if actual := len(table.Entries); actual != 2 {
 				t.Fatalf("Expected 2 entries but got %d", actual)
 			}
-			if expected, actual := uint64(1), table.Entries[1].Frequency; expected != actual {
+			if expected, actual := uint64(1), table.Entries[indexB].Frequency; expected != actual {
 				t.Fatalf("Expected frequency of %d but got %d", expected, actual)
 			}
 			if expected, actual := uint64(2), table.TotalFrequency; expected != actual {
@@ -46,6 +51,40 @@ func TestTable(t *testing.T) {
 			}
 			if expected, actual := uint64(2), table.TotalFrequency; expected != actual {
 				t.Fatalf("Expected total frequency of %d but got %d", expected, actual)
+			}
+		})
+
+		t.Run("keeps entries sorted from most to least frequent", func(t *testing.T) {
+			table := EmptyTable()
+			table.Add(StringSymbol("a"))
+			table.Add(StringSymbol("b"))
+			table.Add(StringSymbol("c"))
+			table.Add(StringSymbol("b"))
+			table.Add(StringSymbol("d"))
+			table.Add(StringSymbol("d"))
+			table.Add(StringSymbol("d"))
+
+			scenarios := []struct {
+				ExpectedSymbol    Symbol
+				ExpectedFrequency uint64
+			}{
+				{StringSymbol("d"), uint64(3)},
+				{StringSymbol("b"), uint64(2)},
+				{StringSymbol("a"), uint64(1)},
+				{StringSymbol("c"), uint64(1)},
+			}
+
+			if expected, actual := len(scenarios), len(table.Entries); expected != actual {
+				t.Fatalf("Expected %d entries but got %d", expected, actual)
+			}
+
+			for i, scenario := range scenarios {
+				if actual := table.Entries[i].Symbol; scenario.ExpectedSymbol != actual {
+					t.Errorf("Expected symbol '%v' in position %d but got '%v'", scenario.ExpectedSymbol, i, actual)
+				}
+				if actual := table.Entries[i].Frequency; scenario.ExpectedFrequency != actual {
+					t.Errorf("Expected frequency of %d in position %d but got %d", scenario.ExpectedFrequency, i, actual)
+				}
 			}
 		})
 	})
